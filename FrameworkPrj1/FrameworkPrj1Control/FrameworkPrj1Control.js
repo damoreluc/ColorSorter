cdebb98582c4c599a4e8f803661f50ed05bab48e
34346 <34346@ITTCV-SIS-24>
2023-12-11 18:51:55 +0100

    aggiunto HMI

---
+++ FrameworkPrj1/FrameworkPrj1Control/FrameworkPrj1Control.js
@@ -0,0 +1,96 @@
+/*
+ * Generated 11/12/2023 09:18:43
+ * Copyright (C) 2023
+ */
+var TcHmi;
+(function (TcHmi) {
+    let Controls;
+    (function (Controls) {
+        let FrameworkPrj1;
+        (function (FrameworkPrj1) {
+            class FrameworkPrj1Control extends TcHmi.Controls.System.TcHmiControl {
+                /*
+                Attribute philosophy
+                --------------------
+                - Local variables are not set while definition in class, so they have the value 'undefined'.
+                - On compile the Framework sets the value from HTML or from theme (possibly 'null') via normal setters.
+                - The "changed detection" in the setter will result in processing the value only once while compile.
+                - Attention: If we have a Server Binding on an Attribute the setter will be called once with null to initialize and later with the correct value.
+                */
+                /**
+                 * Constructor of the control
+                 * @param {JQuery} element Element from HTML (internal, do not use)
+                 * @param {JQuery} pcElement precompiled Element (internal, do not use)
+                 * @param {TcHmi.Controls.ControlAttributeList} attrs Attributes defined in HTML in a special format (internal, do not use)
+                 * @returns {void}
+                 */
+                constructor(element, pcElement, attrs) {
+                    /** Call base class constructor */
+                    super(element, pcElement, attrs);
+                }
+                /**
+                  * If raised, the control object exists in control cache and constructor of each inheritation level was called.
+                  */
+                __previnit() {
+                    // Fetch template root element
+                    this.__elementTemplateRoot = this.__element.find('.TcHmi_Controls_FrameworkPrj1_FrameworkPrj1Control-Template');
+                    if (this.__elementTemplateRoot.length === 0) {
+                        throw new Error('Invalid Template.html');
+                    }
+                    // Call __previnit of base class
+                    super.__previnit();
+                }
+                /**
+                 * Is called during control initialize phase after attribute setter have been called based on it's default or initial html dom values.
+                 * @returns {void}
+                 */
+                __init() {
+                    super.__init();
+                }
+                /**
+                * Is called by the system after the control instance gets part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __attach() {
+                    super.__attach();
+                    /**
+                     * Initialize everything which is only available while the control is part of the active dom.
+                     */
+                }
+                /**
+                * Is called by the system after the control instance is no longer part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __detach() {
+                    super.__detach();
+                    /**
+                     * Disable everything which is not needed while the control is not part of the active dom.
+                     * No need to listen to events for example!
+                     */
+                }
+                /**
+                * Destroy the current control instance.
+                * Will be called automatically if system destroys control!
+                */
+                destroy() {
+                    /**
+                    * While __keepAlive is set to true control must not be destroyed.
+                    */
+                    if (this.__keepAlive) {
+                        return;
+                    }
+                    super.destroy();
+                    /**
+                    * Free resources like child controls etc.
+                    */
+                }
+            }
+            FrameworkPrj1.FrameworkPrj1Control = FrameworkPrj1Control;
+        })(FrameworkPrj1 = Controls.FrameworkPrj1 || (Controls.FrameworkPrj1 = {}));
+    })(Controls = TcHmi.Controls || (TcHmi.Controls = {}));
+})(TcHmi || (TcHmi = {}));
+/**
+* Register Control
+*/
+TcHmi.Controls.registerEx('FrameworkPrj1Control', 'TcHmi.Controls.FrameworkPrj1', TcHmi.Controls.FrameworkPrj1.FrameworkPrj1Control);
+//# sourceMappingURL=FrameworkPrj1Control.js.map
